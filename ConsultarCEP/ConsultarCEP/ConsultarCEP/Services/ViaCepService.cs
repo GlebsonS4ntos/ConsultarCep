3d3389199ea793d681874f220026743c90b5d2e8
Glebson Santos <glebsonsantos@outlook.pt>
2023-05-21 21:27:37 -0300

    Adicionando A entidade Endereco e o Servico que ira pegar o endereco da api da ViaCep

---
+++ ConsultarCEP/ConsultarCEP/ConsultarCEP/Services/ViaCepService.cs
@@ -0,0 +1,23 @@
+﻿using ConsultarCEP.Services.Entitys;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ConsultarCEP.Services
+{
+    public class ViaCepService
+    {
+        private static string Url = "viacep.com.br/ws/{0}/json";
+
+        public static Endereco BuscarEnderecoPorCep(string cep)
+        {
+            string novaUrl = String.Format(Url, cep);
+            WebClient wc = new WebClient();
+
+            string conteudo = wc.DownloadString(novaUrl);
+            return JsonConvert.DeserializeObject<Endereco>(conteudo);
+        }
+    }
+}
