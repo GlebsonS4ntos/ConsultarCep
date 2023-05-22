aff5d49a2065605a6696b564f6706ac526f6f266
Glebson Santos <glebsonsantos@outlook.pt>
2023-05-22 11:49:01 -0300

    Adicionando validações de tamanho do cep digitado e se existe algum caracter que n seja numero

--- ConsultarCEP/ConsultarCEP/ConsultarCEP/MainPage.xaml.cs
+++ ConsultarCEP/ConsultarCEP/ConsultarCEP/MainPage.xaml.cs
@@ -22,19 +22,36 @@ namespace ConsultarCEP
 
         private void ExecultarBuscaCep(Object sender, EventArgs args)
         {
-            
-            Endereco end = ViaCepService.BuscarEnderecoPorCep(CEP_DIGITADO.Text.Trim());
 
-            CEP_DIGITADO.Text = "";
+            if (CepIsValid(CEP_DIGITADO.Text.Trim()))
+            {
+                Endereco end = ViaCepService.BuscarEnderecoPorCep(CEP_DIGITADO.Text.Trim());
 
-            
-            LOGRADOURO.Text = String.IsNullOrEmpty(end.logradouro)? "LOGRADOURO: Não Encontrado" : $"LOGRADOURO: {end.logradouro}";
-            BAIRRO.Text = String.IsNullOrEmpty(end.bairro) ? "BAIRRO: Não Encontrado" : $"BAIRRO: {end.bairro}";
-            LOCALIDADE.Text = $"LOCALIDADE: {end.localidade}";
-            UF.Text = $"UF: {end.uf}";
-            CEP.Text = $"CEP: {end.cep}";
+                CEP_DIGITADO.Text = "";
 
+                LOGRADOURO.Text = String.IsNullOrEmpty(end.logradouro) ? "LOGRADOURO: Não Encontrado" : $"LOGRADOURO: {end.logradouro}";
+                BAIRRO.Text = String.IsNullOrEmpty(end.bairro) ? "BAIRRO: Não Encontrado" : $"BAIRRO: {end.bairro}";
+                LOCALIDADE.Text = $"LOCALIDADE: {end.localidade}";
+                UF.Text = $"UF: {end.uf}";
+                CEP.Text = $"CEP: {end.cep}";
+            }
+        }
 
+        private bool CepIsValid(string cep)
+        {
+            if (cep.Length != 8)
+            {
+                DisplayAlert("Erro", "Verifique o Cep digitado, ele precisa ter 8 caracteres !!", "Ok");
+                return false;
+            }
+            int resultadoCep;
+            if(!int.TryParse(cep,out resultadoCep))
+            {
+                DisplayAlert("Erro", "Verifique o Cep digitado, ele deve conter apenas Números !!", "Ok");
+                return false;
+            }
+
+            return true;
         }
     }
 }
